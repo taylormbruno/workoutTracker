78321fea387c958883f014f4fe5f3e5ccda91f4e
Taylor Bruno <taylorbruno895@gmail.com>
2020-02-19 12:53:56 -0500

    fixed issue with routes not connecting to frontend

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,47 +1,49 @@
 const router = require('express').Router();
 const Workout = require('../models/workout.js');
 
-module.exports = function() {
-    // getLastWorkout
-    router.get('/api/workouts', (req, res) => {
-        Workout.find({})
-            .sort({ date: -1})
-            .then( dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.status(400).json(err);
-            });
-    });
+// module.exports = function() {
+// getLastWorkout
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    // .sort({ date: -1})
+        .then( dbWorkout => {
+            console.log(dbWorkout);
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
     
-    // createWorkout
-    router.post('/api/workouts', ({ body }, res) => {
-        console.log('------Line 19------');
-        console.log(body);
-        Workout.create(body)
-            .then(newWorkout => {
-                res.json(newWorkout);
-            })
-            .catch (err => {
-                res.status(400).json(err);
-            });
-    });
+// createWorkout
+router.post('/api/workouts', ({ body }, res) => {
+    console.log('------Line 19------');
+    console.log(body);
+    Workout.create(body)
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch (err => {
+            res.status(400).json(err);
+        });
+});
 
-    // addExercise
-    router.put('/api/workouts/:id', ({ body }, res) => {
-        console.log('------Line 32------');
-        console.log(body);
-        // let id = req.params.id;
-        let updWorkout;
-        res.json(updWorkout);
-    });
+// addExercise
+router.put('/api/workouts/:id', ({ body }, res) => {
+    console.log('------Line 32------');
+    console.log(body);
+    // let id = req.params.id;
+    let updWorkout;
+    res.json(updWorkout);
+});
 
-    // getWorkoutsInRange
-    router.get('/api/workouts/range', (req, res) => {
-        console.log('------Line 41------');
-        console.log(req.body);
-        let range;
-        res.json(range);
-    });
+// getWorkoutsInRange
+router.get('/api/workouts/range', (req, res) => {
+    console.log('------Line 41------');
+    console.log(req.body);
+    let range;
+    res.json(range);
+});
 
-};
+// };
+module.exports = router;
